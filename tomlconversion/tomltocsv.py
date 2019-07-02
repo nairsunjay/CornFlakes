4d589072268ec17939e9fcc729320684d7d3638f
sunjaynair <nairsunjay@gmail.com>
2019-07-02 16:45:41 +0200

    Created a new folder to put all the contents of the code

---
+++ tomlconversion/tomltocsv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+import csv
+import os
+import re
+
+
+
+file_object = open("/Users/sunjaynair/PycharmProjects/CornFlakes/tomlconversion/text.toml" , 'r')
+
+
+
+
+
+print("sunjay nair")
+
